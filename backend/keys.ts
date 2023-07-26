f4fca79f1ab0208306b8c8c325dfa11c0813bc99
Sean Murphy <seanm519@icloud.com>
2023-07-25 20:16:31 -0400

    blash

---
+++ backend/keys.ts
@@ -0,0 +1,5 @@
+const username: string = "dienttran7";
+const password: string = "5ySbdWv93iD91i9W";
+
+export const uri_key: string = 'mongodb+srv://dienttran7:5ySbdWv93iD91i9W@cluster0.wvwrgjf.mongodb.net/?retryWrites=true&w=majority';
+export const secret_key: string = 'iloveblueberrys';
---
+++ dist/keys.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.secret_key = exports.uri_key = void 0;
+const username = "dienttran7";
+const password = "5ySbdWv93iD91i9W";
+exports.uri_key = 'mongodb+srv://dienttran7:5ySbdWv93iD91i9W@cluster0.wvwrgjf.mongodb.net/?retryWrites=true&w=majority';
+exports.secret_key = 'iloveblueberrys';
